2c9f1e1d18e73f52df2bf6ff508361d15d8772a1
jonathan uriel paredes martinez <58749109+andorsdk@users.noreply.github.com>
2024-02-22 12:56:27 -0600

    Create calculadora.c
    funcion principal

---
+++ calculadora.c
@@ -0,0 +1,11 @@
+//calculadora en c
+#include <stdio.h>
+
+int main() {
+    char operador;
+    double num1, num2, resultado;
+     printf("Ingrese un operador (+, -, *, /): ");
+    scanf("%c", &operador);
+     printf("Ingrese dos números separados por espacio: ");
+    scanf("%lf %lf", &num1, &num2);
+    }
