7c2104dd48f2abc0288da17aac7262353ad6366a
jonathan uriel paredes martinez <58749109+andorsdk@users.noreply.github.com>
2024-02-22 13:04:31 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -4,8 +4,30 @@
 int main() {
     char operador;
     double num1, num2, resultado;
-     printf("Ingrese un operador (+, -, *, /): ");
+    printf("Ingrese un operador (+, -, *, /): ");
     scanf("%c", &operador);
-     printf("Ingrese dos números separados por espacio: ");
+    if(operador== '+' || operador== '-' || operador== '/' || operador== '*'){
+    printf("Ingrese dos números separados por espacio: ");
     scanf("%lf %lf", &num1, &num2);
+
     }
+    switch (operador) {
+       
+            break;
+        case '*':
+            resultado = num1 * num2;
+            break;
+        case '/':
+            
+            if (num2 != 0) {
+                resultado = num1 / num2;
+            } else {
+                printf("Error: División entre  cero\n");
+                return 1;  
+            }
+            break;
+        default:
+            printf("Error: Operador no válido\n");
+            return 1;  // Salir del programa 
+    }
+}
